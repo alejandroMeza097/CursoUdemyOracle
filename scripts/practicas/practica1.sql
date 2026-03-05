421bad9fdf287f687f0ad9d1767037f52d8729f9
Alex Meza <alex.meza.5471@gmail.com>
2026-03-04 21:44:51 -0600

    Commit inicial. Se genera proyecto de curso de oracle

---
+++ scripts/practicas/practica1.sql
@@ -0,0 +1,15 @@
+--Visualizar el nombre y el número de teléfono de los empleados
+SELECT FIRST_NAME AS NOMBRE, PHONE_NUMBER AS TELEFONO FROM EMPLOYEES;
+
+--Visualizar el nombre y el tipo de trabajo de los empleados (FIRST_NAME, JOB_ID). Debe aparecer en la cabecera NOMBRE Y Tipo de Trabajo.
+SELECT FIRST_NAME AS NOMBRE, JOB_ID AS "Tipo de Trabajo" FROM EMPLOYEES;
+
+--Selecciona todas las columnas de la tabla REGIONS
+SELECT * FROM REGIONS;
+
+--Indicar los nombres de los países de la tabla COUNTRIES
+SELECT COUNTRY_NAME AS "NOMBRE DE PAISES" FROM COUNTRIES;
+
+--Seleccionar las columnas STREET_ADDRESS, CITY, STATE_PROVINCE de la table LOCATIONS. 
+-- Debemos poner las columnas como dirección, Ciudad y Estado
+SELECT STREET_ADDRESS AS "dirección", CITY AS "Ciudad", STATE_PROVINCE AS "Estado" FROM LOCATIONS;
