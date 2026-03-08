d6007d9d7a5510bd03d295124963f47203c7bcdb
Alex Meza <alex.meza.5471@gmail.com>
2026-03-08 17:57:26 -0600

    Solucion a practica 3 sobre concatenacion y creacion de carpeta para notas

---
+++ scripts/ejercicios/literales.sql
@@ -0,0 +1,2 @@
+--Manejo de la concatenacion
+SELECT 'NOMBRE COMPLETO : ' || FIRST_NAME ||  ' ' ||LAST_NAME AS "NOMBRE" FROM EMPLOYEES;
---
+++ scripts/practicas/practica3.sql
@@ -0,0 +1,3 @@
+SELECT 'El empleado ' || FIRST_NAME || ' del departamento ' || DEPARTMENT_ID || ' tiene un salario de ' || SALARY AS "DATOS" FROM EMPLOYEES;
+
+SELECT 'La calle ' || STREET_ADDRESS || ' pertenece a la ciudad :' || CITY AS "Calle y Ciudad" FROM LOCATIONS;
